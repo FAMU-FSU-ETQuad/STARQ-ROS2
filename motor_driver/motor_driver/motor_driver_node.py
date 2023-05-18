360935cb8c937e96979aa735aceb3913a622f61e
jtylerboylan <jtylerboylan@outlook.com>
2023-05-18 11:14:31 -0400

    GPT fixes

--- motor_driver/motor_driver/motor_driver_node.py
+++ motor_driver/motor_driver/motor_driver_node.py
@@ -1,5 +1,6 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.executors import SingleThreadedExecutor
 
 from std_msgs.msg import Float32MultiArray
 from std_msgs.msg import Int32MultiArray
@@ -8,15 +9,16 @@ from std_srvs.srv import Empty
 import asyncio
 import moteus
 import math
+import threading
 
 # Boom motors
 MOTOR_IDS = [9, 10]
 
 # Dina's values
-MOTOR_KP = 127.8
-MOTOR_KI = 0.0
-MOTOR_KD = 2.25
-MOTOR_FLUX_BRAKE = 35.5
+MOTOR_KP = [127.8, 25.0]
+MOTOR_KI = [0.0, 0.0]
+MOTOR_KD = [2.25, 0.12] 
+MOTOR_FLUX_BRAKE = [35.5, 35.5]
 ZERO_POSITIONS = [0.0, 0.5]
 
 REZERO_ON_START = True
@@ -25,9 +27,7 @@ class MotorDriverNode(Node):
 
     def __init__(self):
         super().__init__('motor_driver_node')
-        asyncio.run(self.main())
 
-    async def main(self):
         # Declare ROS parameters
         self.declare_parameters(
             namespace='',
@@ -46,17 +46,14 @@ class MotorDriverNode(Node):
         (p_motor_ids, p_motor_kp, p_motor_ki, p_motor_kd, p_motor_flux_brake, p_zero_positions, p_rezero_on_start) = self.get_parameters(
             ['motor_ids', 'motor_kp', 'motor_ki', 'motor_kd', 'motor_flux_brake', 'zero_positions', 'rezero_on_start'])
         self.motor_ids = p_motor_ids.get_parameter_value().integer_array_value
-        self.motor_kp = p_motor_kp.get_parameter_value().double_value
-        self.motor_ki = p_motor_ki.get_parameter_value().double_value
-        self.motor_kd = p_motor_kd.get_parameter_value().double_value
-        self.motor_flux_brake = p_motor_flux_brake.get_parameter_value().double_value
+        self.motor_kp = p_motor_kp.get_parameter_value().double_array_value
+        self.motor_ki = p_motor_ki.get_parameter_value().double_array_value
+        self.motor_kd = p_motor_kd.get_parameter_value().double_array_value
+        self.motor_flux_brake = p_motor_flux_brake.get_parameter_value().double_array_value
         self.zero_positions = p_zero_positions.get_parameter_value().double_array_value
         self.rezero_on_start = p_rezero_on_start.get_parameter_value().bool_value
 
         self.get_logger().info('Parameters Saved.')
-
-        # Initialize controllers
-        await self.init_controllers()
 
         # --- SUBSCRIBERS ---
 
@@ -64,7 +61,7 @@ class MotorDriverNode(Node):
         self.cmd_pos_sub = self.create_subscription(
             Float32MultiArray,
             '/starq/motors/cmd/position',
-            self.set_position,
+            self.set_position_callback,
             10
         )
 
@@ -72,7 +69,7 @@ class MotorDriverNode(Node):
         self.cmd_vel_sub = self.create_subscription(
             Float32MultiArray,
             '/starq/motors/cmd/velocity',
-            self.set_velocity,
+            self.set_velocity_callback,
             10
         )
 
@@ -128,17 +125,13 @@ class MotorDriverNode(Node):
 
         self.get_logger().info('Services created.')
 
-        # -------------------
+        # --- TIMERS ---
 
         # Sample motor information
         info_sample_rate = 0.1 # seconds
-        self.info_timer = self.create_timer(info_sample_rate, self.publish_info)
+        self.info_timer = self.create_timer(info_sample_rate, self.publish_info_callback)
 
-        # Done initializing
-        self.get_logger().info('Motor Driver Initialized.')
-
-    # Initialize motor controllers
-    async def init_controllers(self):
+        # --- MOTORS ---
 
         # Use FDCANUSB (Power distribution board) for comm
         self.transport = moteus.Fdcanusb()
@@ -158,6 +151,20 @@ class MotorDriverNode(Node):
         }
 
         self.get_logger().info('Streams Initialized.')
+
+        # Done initializing
+        self.get_logger().info('Motor Driver Initialized.')
+
+    @classmethod
+    async def create(cls):
+        self = MotorDriverNode()
+
+        # asynchronous initialization
+        await self.main()
+
+        return self
+
+    async def main(self):
         
         # Reset motor faults
         await self.reset_faults()
@@ -172,11 +179,10 @@ class MotorDriverNode(Node):
 
         self.get_logger().info('Motors Initialized.')
 
-    # Reset faults function
-    async def reset_faults(self):
-        await self.transport.cycle([servo.make_stop() for servo in self.servos.values()])
-
     # Set position command callback
+    def set_position_callback(self, msg):
+        asyncio.create_task(self.set_position(msg))
+
     async def set_position(self, msg):
         commands = {
             servo.make_position( 
@@ -188,6 +194,9 @@ class MotorDriverNode(Node):
         await self.transport.cycle(commands)
 
     # Set velocity command callback
+    def set_velocity_callback(self, msg):
+        asyncio.create_task(self.set_velocity(msg))
+
     async def set_velocity(self, msg):
         commands = {
             servo.make_position( 
@@ -198,57 +207,98 @@ class MotorDriverNode(Node):
         }
         await self.transport.cycle(commands)
 
-    # Sample motor information callback
+    # Publish motor information callback
+    def publish_info_callback(self):
+        asyncio.create_task(self.publish_info())
+
     async def publish_info(self):
+        # Create a mapping of publisher to data array
+        pub_data_map = {
+            self.info_position_pub: Float32MultiArray(),
+            self.info_velocity_pub: Float32MultiArray(),
+            self.info_torque_pub: Float32MultiArray(),
+            self.info_qcurrent_pub: Float32MultiArray(),
+            self.info_temp_pub: Float32MultiArray(),
+            self.info_fault_pub: Int32MultiArray(),
+        }
 
-        positions = Float32MultiArray()
-        velocities = Float32MultiArray()
-        torques = Float32MultiArray()
-        qcurrents = Float32MultiArray()
-        temps = Float32MultiArray()
-        faults = Int32MultiArray()
+        # Create a mapping of register to publisher
+        reg_pub_map = {
+            moteus.Register.POSITION: self.info_position_pub,
+            moteus.Register.VELOCITY: self.info_velocity_pub,
+            moteus.Register.TORQUE: self.info_torque_pub,
+            moteus.Register.Q_CURRENT: self.info_qcurrent_pub,
+            moteus.Register.TEMPERATURE: self.info_temp_pub,
+            moteus.Register.FAULT: self.info_fault_pub,
+        }
 
+        # Query all servo states
         results = await self.transport.cycle(servo.make_query() for servo in self.servos.values())
+        
+        # Process each result
         for result in results:
-            positions.data.append(result.values[moteus.Register.POSITION])
-            velocities.data.append(result.values[moteus.Register.VELOCITY])
-            torques.data.append(result.values[moteus.Register.TORQUE])
-            qcurrents.data.append(result.values[moteus.Register.Q_CURRENT])
-            temps.data.append(result.values[moteus.Register.TEMPERATURE])
-            faults.data.append(result.values[moteus.Register.FAULT])
+            for reg, pub in reg_pub_map.items():
+                pub_data_map[pub].data.append(result.values[reg])
 
-        self.info_position_pub.publish(positions)
-        self.info_velocity_pub.publish(velocities)
-        self.info_torque_pub.publish(torques)
-        self.info_qcurrent_pub.publish(qcurrents)
-        self.info_temp_pub.publish(temps)
-        self.info_fault_pub.publish(faults)
+        # Publish all messages
+        for pub, msg in pub_data_map.items():
+            pub.publish(msg)
+
+
+
+    # Reset faults function
+    async def reset_faults(self):
+        await self.transport.cycle([
+            servo.make_stop() 
+            for servo in self.servos.values()
+        ])
 
     # Set motor gains
     async def set_gains(self, KP, KI, KD):
         for idx, stream in self.streams.items():
-            await stream.command(bytes(str("conf set servo.pid_position.kp " + str(KP)), 'utf-8'))
-            await stream.command(bytes(str("conf set servo.pid_position.ki " + str(KI)), 'utf-8'))
-            await stream.command(bytes(str("conf set servo.pid_position.kd " + str(KD)), 'utf-8'))
+            await stream.command(bytes(str("conf set servo.pid_position.kp " + str(KP[idx])), 'utf-8'))
+            await stream.command(bytes(str("conf set servo.pid_position.ki " + str(KI[idx])), 'utf-8'))
+            await stream.command(bytes(str("conf set servo.pid_position.kd " + str(KD[idx])), 'utf-8'))
         
     # Set flux brake
     async def set_flux_brake(self, V):
-        for stream in self.streams.values():
-            await stream.command(bytes("conf set servo.flux_brake_min_voltage " + str(V), 'utf-8'))
+        for idx, stream in self.streams.items():
+            await stream.command(bytes("conf set servo.flux_brake_min_voltage " + str(V[idx]), 'utf-8'))
 
     # Set motor positions to zero
     async def set_as_zero(self, request, response):
-        for idx, servo in self.servos.items():
-            await servo.set_output_exact(position = self.zero_positions[idx])
-        return response
+        await self.transport.cycle([
+            servo.make_set_output_exact(position = self.zero_positions[idx])
+            for idx, servo in self.servos.items()
+        ])
 
 # ROS Entry
 def main(args=None):
     rclpy.init(args=args)
-    motor_driver = MotorDriverNode()
-    rclpy.spin(motor_driver)
-    motor_driver.destroy_node()
-    rclpy.shutdown()
+
+    loop = asyncio.get_event_loop()
+    motor_driver_node = loop.run_until_complete(MotorDriverNode.create())
+
+    executor = SingleThreadedExecutor()
+    executor.add_node(motor_driver_node)
+
+    def spin():
+        while rclpy.ok():
+            executor.spin_once(timeout_sec=0.1)
+
+    spin_thread = threading.Thread(target=spin)
+    spin_thread.start()
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        motor_driver_node.destroy_node()
+        rclpy.shutdown()
+        spin_thread.join()
 
 if __name__ == '__main__':
     main()
+
+
