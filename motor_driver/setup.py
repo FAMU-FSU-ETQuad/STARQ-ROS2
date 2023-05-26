38781901c9c4953bffb8e4d7dc33c0b1572b55f6
jtylerboylan <jtylerboylan@outlook.com>
2023-05-26 12:31:37 -0400

    ODrive updates

--- motor_driver/setup.py
+++ motor_driver/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import os
 
 package_name = 'motor_driver'
 
@@ -8,8 +9,11 @@ setup(
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+            [os.path.join('resource', 'ament_python')]),
         ('share/' + package_name, ['package.xml']),
+        # Add this line for your launch file
+        (os.path.join('share', package_name, 'launch'), ['launch/motor_driver.launch.py']),
+        (os.path.join('share', package_name, 'config'), ['config/motors.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
