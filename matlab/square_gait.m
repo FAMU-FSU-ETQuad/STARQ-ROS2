bff492fd9efd7fb5ae55ccf0d6b1eccfcc16d8d3
jtylerboylan <jtylerboylan@outlook.com>
2023-06-16 15:38:30 -0400

    Fix LL

--- matlab/square_gait.m
+++ matlab/square_gait.m
@@ -20,9 +20,9 @@ N = 500;
 publish_rate = f * N
 
 % Square dimensions
-width = (L1+L2)/2;
-height = (L1+L2)/2;
-y_offset = -(L1+L2)/2;
+width = 50;
+height = 50;
+y_offset = -160;
 
 top = [linspace(-width/2, width/2, N/4);
      linspace(height/2 + y_offset, height/2 + y_offset, N/4);
